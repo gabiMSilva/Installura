771e51da898cbc497318a134bbe740c39dfcc379
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2019-12-11 19:22:09 -0300

    Created main feed

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -8,106 +8,67 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
+  Image,
   Text,
-  StatusBar,
+  View,
+  Dimensions,
+  FlatList,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {} from 'react-native/Libraries/NewAppScreen';
+
+const fotos = [
+  {id: 1, usuario: 'rafael'},
+  {id: 2, usuario: 'alberto'},
+  {id: 3, usuario: 'vitor'},
+];
+
+const width = Dimensions.get('screen').width;
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <FlatList
+      style={styles.container}
+      data={fotos}
+      keyExtractor={item => item.id}
+      renderItem={({item}) => (
+        <View>
+          <View style={styles.cabecalho}>
+            <Image
+              source={require('./resources/img/alura.jpg')}
+              style={styles.fotoDePerfil}
+            />
+            <Text>{item.usuario}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <Image
+            source={require('./resources/img/alura.jpg')}
+            style={styles.foto}
+          />
+        </View>
+      )}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    marginTop: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  cabecalho: {
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
+  fotoDePerfil: {
+    marginRight: 10,
+    borderRadius: 20,
+    width: 40,
+    height: 40,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  foto: {
+    width: width,
+    height: width,
   },
 });
 
