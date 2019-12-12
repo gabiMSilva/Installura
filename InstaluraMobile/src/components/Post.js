a37d0b6275ba719aee973e0b408cbc6b1f4b3f64
Gabriela Medeiros <gabriela@loopce.com>
2019-12-12 14:09:04 -0300

    Added like button

--- InstaluraMobile/src/components/Post.js
+++ InstaluraMobile/src/components/Post.js
@@ -5,25 +5,52 @@ import {
   Image,
   Dimensions,
   Text,
+  TouchableOpacity
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { foto: this.props.foto }
+  }
+
+  carregaIcone(likeada){
+    return likeada ? require('../../resources/img/s2-checked.png') : require('../../resources/img/s2.png');
+  }
+
+  like(){
+    const fotoAtualizada = {
+      ...this.state.foto,
+      likeada: !this.state.foto.likeada
+    };
+  
+    this.setState({foto: fotoAtualizada});
+  }
+
   render() {
+    const { foto } = this.state;
+
     return (
       <View>
         <View style={styles.cabecalho}>
           <Image 
-              source={{uri: this.props.foto.urlPerfil}}
+              source={{uri: foto.urlPerfil}}
               style={styles.fotoDePerfil}
           />
-          <Text>{this.props.foto.loginUsuario}</Text>
+          <Text>{foto.loginUsuario}</Text>
         </View>
         <Image 
-            source={{uri: this.props.foto.urlFoto}}
+            source={{uri: foto.urlFoto}}
             style={styles.foto}
         />
+        <View style={styles.rodape}>
+          <TouchableOpacity onPress={this.like.bind(this)}>
+            <Image style={styles.botaoDeLike} source={this.carregaIcone(foto.likeada)} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -48,5 +75,14 @@ const styles = StyleSheet.create({
     width: width,
     height: width
   },
+
+  botaoDeLike: {
+    width: 40,
+    height: 40
+  },
+
+  rodape: {
+    margin: 10
+  }
 
 });
---
+++ ios/Pods/Target Support Files/Pods-Installura/Pods-Installura-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Installura : NSObject
+@end
+@implementation PodsDummy_Pods_Installura
+@end
